e3a3a3fd36654ab84a9948d40788086f1204c6c3
Zane Gomes <zgom0003@student.monash.edu>
2021-11-19 19:18:47 +0300

    saw a solution video, then read the solution posted on the site.

--- CodeForces/1000/minimumExtraction.cpp
+++ CodeForces/1000/minimumExtraction.cpp
@@ -1,28 +1,19 @@
 #include <iostream>
 #include <algorithm>
 
-/*
-On the third round of the outermost loop in main (i = 3), there is some weird ness and
-the first element of the result array is changed to some random number.
-*/
-
 using namespace std;
 
-int generateResult(int array[], const int arraySize)
+int generateResult(long long int array[], const int arraySize)
 {
-    int min[arraySize] = {0};
-    int lastIndex = arraySize-1;
+    long long int min[arraySize] = {0};
 
-    for (int i = 0; i < arraySize; i++, array++, lastIndex--)
+    sort(array, array+arraySize);
+
+    min[0] = array[0];
+
+    for (int i = 1; i < arraySize; i++)
     {
-        sort(array, array+lastIndex+1);
-
-        min[i] = array[0];
-
-        if (lastIndex == 0) break;
-
-        for (int j = lastIndex; j >= 0; j--)
-            array[j] -= array[0];
+        min[i] = array[i] - array[i-1];
     }
 
     return *max_element(min, min+arraySize);
@@ -30,20 +21,23 @@ int generateResult(int array[], const int arraySize)
 
 int main(){
 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
     //get the initial input
     int numTestCases = 0;
     cin >> numTestCases;
 
     int result[numTestCases] = {0};
 
-    int sizeOfArray = 0;
+    long long int sizeOfArray = 0;
 
     //get the length and array elements
     for (int i = 0; i < numTestCases; i++)
     {
         cin >> sizeOfArray;
         
-        int array[sizeOfArray] = {0};
+        long long int array[sizeOfArray] = {0};
 
         for (int j = 0; j < sizeOfArray; j++)
             cin >> array[j];
@@ -53,13 +47,5 @@ int main(){
         if (i < numTestCases-1) cout << '\n';
     }
 
-/*
-    //printing the result
-    for (int i = 0; i < numTestCases; i++) 
-    {
-        cout << result[i];
-        if (i < numTestCases-1) cout << '\n';
-    }
-*/
     return 0;
 }
