0384e83af731739b1193537f98f1458457f37bae
Zane Gomes <zgom0003@student.monash.edu>
2021-11-19 12:41:15 +0300

    working on the algrothim

--- CodeForces/1000/minimumExtraction.cpp
+++ CodeForces/1000/minimumExtraction.cpp
@@ -1,64 +1,25 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 
 using namespace std;
 
-/*
-//not sure why this works but whatever ig
-int generateResult(int* array, const int arraySize)
-{
-    int arrayMin[arraySize] = {};
-
-    for (int k = arraySize; k > 1; k--)
-    {
-        //int min = *array;
-
-
-        for (int i = 0; i < k ; i++)
-            if (*(array+i) < min) min = array[i];
-
-        int resultArray[k-1] = {};
-
-        for (int i = 0, j = 0; i < k ; i++)
-        {
-            *(array+i) -= arrayMin[arraySize-k];
-
-            if (*(array+i) == 0) continue;
-
-            resultArray[j] = *(array+i);
-            j++;
-        }
-
-        for (int i = 0; i < arraySize-1; i++)
-            *(array+i) = resultArray[i];
-    }
-
-    arrayMin[arraySize-1] = *array;
-
-    int maximal = arrayMin[0];
-
-    for (int i = 0; i < arraySize; i++) 
-        if (arrayMin[i] > maximal) maximal = arrayMin[i];
-
-    return maximal;
-}
-*/
-
 int generateResult(int array[], const int arraySize)
 {
-    int min[arraySize] = {0};
+    //delete the temp array later
 
-    for (int i = 0; i < arraySize; i++)
+    int min[arraySize] = {0};
+    int lastIndex = arraySize-1;
+
+    for (int i = 0; i < arraySize; i++, array++, lastIndex--)
     {
         sort(array, array+arraySize);
 
         min[i] = array[0];
 
-        for (int i = arraySize; i > 0; i--)
-            array[i] -= array[0];
+        if (lastIndex == 0) break;
 
-        array += 1;
+        for (int j = lastIndex; j >= 0; j--)
+            array[j] -= array[0];
     }
 
     return *max_element(min, min+arraySize);
@@ -68,31 +29,29 @@ int main(){
 
     //get the initial input
     int numTestCases = 0;
-
     cin >> numTestCases;
 
-    int result[numTestCases] = {};
+    int result[numTestCases] = {0};
+
+    int sizeOfArray = 0;
 
     //get the length and array elements
     for (int i = 0; i < numTestCases; i++)
     {
-        int sizeOfArray = 0;
         cin >> sizeOfArray;
         
-        int array[sizeOfArray] = {};
+        int array[sizeOfArray] = {0};
 
         for (int j = 0; j < sizeOfArray; j++)
-        {
             cin >> array[j];
-        }
 
         result[i] = generateResult(array, sizeOfArray);
     }
 
     for (int i = 0; i < numTestCases; i++) 
     {
-            cout << result[i];
-            if (i < numTestCases-1) cout << '\n';
+        cout << result[i];
+        if (i < numTestCases-1) cout << '\n';
     }
 
     return 0;
