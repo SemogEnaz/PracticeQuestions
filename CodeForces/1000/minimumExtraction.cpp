42e52e07dd3b476a048a090723b97256c037c3d3
Zane Gomes <zgom0003@student.monash.edu>
2021-11-19 16:43:22 +0300

    finally I think i finished

--- CodeForces/1000/minimumExtraction.cpp
+++ CodeForces/1000/minimumExtraction.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
 #include <algorithm>
 
+/*
+On the third round of the outermost loop in main (i = 3), there is some weird ness and
+the first element of the result array is changed to some random number.
+*/
+
 using namespace std;
 
 int generateResult(int array[], const int arraySize)
 {
-    //delete the temp array later
-
     int min[arraySize] = {0};
     int lastIndex = arraySize-1;
 
     for (int i = 0; i < arraySize; i++, array++, lastIndex--)
     {
-        sort(array, array+arraySize);
+        sort(array, array+lastIndex+1);
 
         min[i] = array[0];
 
@@ -45,14 +48,18 @@ int main(){
         for (int j = 0; j < sizeOfArray; j++)
             cin >> array[j];
 
-        result[i] = generateResult(array, sizeOfArray);
+        cout << generateResult(array, sizeOfArray);
+
+        if (i < numTestCases-1) cout << '\n';
     }
 
+/*
+    //printing the result
     for (int i = 0; i < numTestCases; i++) 
     {
         cout << result[i];
         if (i < numTestCases-1) cout << '\n';
     }
-
+*/
     return 0;
 }
---
+++ CodeForces/800/LinearKeyboard.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+int main(){
+
+    int t = 0;
+    cin >> t;
+
+    string keyBoard = "";
+    string word = "";
+
+    int result[t] = {0};
+    int count = 0;
+
+    while (t--)
+    {
+        cin >> keyBoard;
+        cin >> word;
+
+        
+    }
+
+
+
+    return 0;
+}
